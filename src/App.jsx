cf72fb9f7ad96639973fadc51897d2c20e54d696
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-10 15:11:07 +0300

    style: improve content spacing

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
   }
 
   return (
-    <main className="h-screen my-8 flex gap-8">
+    <main className="h-screen my-8 flex gap-8 mr-8">
       <Sidebar onAddProject={startAddProject}/>
       {content}
     </main>
