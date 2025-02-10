0fc1608ea6fecc1109eb22c87bdb1fa46b9f043d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-10 18:32:28 +0300

    feat: capture new task value

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 
 function NewTask() {
+  const [enteredTask, setEnteredTask] = useState()
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value)
+  }
+
   return (
     <div className='flex items-center gap-2'>
         <input type="text" className='w-64 px-2 py-1 rounded-sm bg-stone-200'/>
         <button 
             className='px-2 py-1 rounded-r-xl text-stone-700 hover:text-stone-950 cursor-pointer border border-stone-400 hover:border-stone-700 hover:bg-stone-100'
+            onChange={handleChange}
+            value={enteredTask}
         >
             Add Task
         </button>
