12ba9bf451c7365a1c43d16cae0935cba4161bea
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-10 12:32:47 +0300

    style: add sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <aside>
+        <h2>Your Projetcs</h2>
+        <div>
+            <button>
+               + Add project
+            </button>
+        </div>
+        <ul></ul>
+    </aside>
+  )
+}
+
+export default Sidebar
