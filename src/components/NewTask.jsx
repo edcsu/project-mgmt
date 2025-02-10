1e665ba1f2218a376be61b8139de1d3984f18b6e
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-10 18:28:14 +0300

    feat: add new task component

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NewTask() {
+  return (
+    <div className='flex items-center gap-2'>
+        <input type="text" className='w-64 px-2 py-1 rounded-sm bg-stone-200'/>
+        <button 
+            className='px-2 py-1 rounded-r-xl text-stone-700 hover:text-stone-950 cursor-pointer border border-stone-400 hover:border-stone-700 hover:bg-stone-100'
+        >
+            Add Task
+        </button>
+    </div>
+  )
+}
+
+export default NewTask
