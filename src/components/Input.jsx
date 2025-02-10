e1ddc5848116b37743c54b2ded7bc050c7e6b192
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-10 13:30:58 +0300

    style: add input styles

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+const inputClasses = 'w-full p-1 boeder-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus-border-stone-600'
 
 function Input({label, isTextArea, ...props}) {
   return (
-    <p>
-        <label htmlFor="">{label}</label>
-        {isTextArea ? <textarea name="" {...props}></textarea> : <input type="text" {...props}/>}
+    <p className='flex flex-col gap-1 my-4'>
+        <label className='text-sm font-bold uppercase text-stone-500' htmlFor="">{label}</label>
+        {isTextArea ? 
+            <textarea className={inputClasses} name="" {...props}></textarea> : 
+            <input className={inputClasses} type="text" {...props}/>}
     </p>
   )
 }
